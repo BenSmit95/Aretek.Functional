12db2238dabd9e836194308f3399e86e577676e3
Benjamin <benjismit@live.nl>
2022-07-17 16:08:43 +0200

    Add Apply extension method

---
+++ src/Aretek.Functional.Tests/PartialApplication/ApplyTests.cs
@@ -0,0 +1,18 @@
+﻿using Aretek.Functional.PartialApplication;
+
+namespace Aretek.Functional.Tests.PartialApplication;
+
+public class ApplyTests
+{
+    [Fact(DisplayName = "Apply should apply the first parameter")]
+    public void Apply_Should_Apply_First_Parameter()
+    {
+        var func = (int num1, int num2) => num1 + num2;
+
+        var applied = func.Apply(1);
+        var applied2 = func.Apply(10);
+
+        applied(5).Should().Be(6);
+        applied2(15).Should().Be(25);
+    }
+}
---
+++ src/Aretek.Functional/PartialApplication/ApplyExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Aretek.Functional.PartialApplication;
+
+public static class ApplyExtensions
+{
+    /// <summary>
+    /// Takes the function and returns another function with the first argument applied
+    /// </summary>
+    /// <param name="func">The function</param>
+    /// <param name="p1">The first argument</param>
+    /// <typeparam name="T1">The first parameter type</typeparam>
+    /// <typeparam name="T2">The second parameter type</typeparam>
+    /// <typeparam name="TResult">The return type</typeparam>
+    /// <returns>A func that takes the second parameter type and returns the return type</returns>
+    public static Func<T2, TResult> Apply<T1, T2, TResult>(
+        this Func<T1, T2, TResult> func,
+        T1 p1
+    ) => (p2) => func(p1, p2);
+}
